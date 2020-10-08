e75a60b5fc5785b77898d450133591239d67fa57
Ajit Kulkarni <48342806+AjitKulkarni24@users.noreply.github.com>
2020-10-08 23:15:56 +0530

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,21 @@
+largest = None
+smallest = None
+while True:
+    try:
+        num = input("Enter a number: ")
+        if num == "done":
+            break
+        num = int(num)
+
+        if largest is None or largest < num:
+            largest = num
+        elif smallest is None or smallest > num:
+            smallest = num
+    except:
+        print("Invalid input")
+
+
+
+
+print ("Minimum is", smallest)
+print ("Maximum is", largest)
